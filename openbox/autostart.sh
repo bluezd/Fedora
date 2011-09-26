d5ba4d6cdceb3afc1fc8019eebd6f77cd0199b18
Dong Zhu <bluezhudong@gmail.com>
2011-09-26 21:47:19 +0800

    Add WM-openbox ...

---
+++ openbox/autostart.sh
@@ -0,0 +1,8 @@
+#awsetbg /home/zhudong/.config/awesome/themes/wombat/10.jpg
+awsetbg /home/zhudong/.config/awesome/themes/wombat/city.jpg
+
+tint2 &
+gnome-power-manager &
+nm-applet &
+update-notifier &
+
